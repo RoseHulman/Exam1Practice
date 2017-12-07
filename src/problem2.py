fbe0cf8e688d58b45270c16a0ffe4639b6e2780a
petersa <petersa@rose-hulman.edu>
2017-12-06 23:29:21 -0500

    Done

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 1, problem 2.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Abbie Peterson.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -110,6 +110,18 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
+
+    top = rectangle.get_upper_right_corner()
+    bottom = rectangle.get_lower_left_corner()
+    line = rg.
+
+
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
